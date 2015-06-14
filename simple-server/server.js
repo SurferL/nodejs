a6f4108834382039b8d88cdc093a2eafb590bb55
Lawrence Chan <lawrence.chan03@gmail.com>
2015-06-14 20:22:27 +0100

    Added functionality to load different site pages.

--- simple-server/server.js
+++ simple-server/server.js
@@ -1,8 +1,61 @@
+/* Require modules */
 var http = require( 'http' );
+var url = require( 'url' );
+var path = require ( 'path' );
+var fs = require( 'fs' );
 
-http.createServer( function (req, res) {
-  res.writeHead( 200, {'Content-Type': 'text/plain'} );
-  res.end( 'Hello World\n' );
-}).listen( 1337, '127.0.0.1' );
+/* Array of Mime Types*/
+var mimeTypes = {
+	"html" : "text/html",
+	"jpeg" : "image/jpeg",
+	"jpg"  : "image/jpeg",
+	"png"  : "image/png",
+	"js"   : "text/javascript",
+	"css"  : "text/css"
+};
 
-console.log( 'Server running at http://127.0.0.1:1337/' );
+/* Create Server */
+http.createServer( function( req, res ) 
+{
+	var uri = url.parse( req.url ).pathname;
+	var fileName = path.join( process.cwd(), unescape( uri ) );
+	console.log( 'Loading ' + uri );
+	var stats;
+	
+	try
+	{
+		stats = fs.lstatSync( fileName );
+	} 
+	catch( e ) 
+	{
+		res.writeHead( 404, {'Content-type': 'text/plain'} );
+		res.write( '404 Not Found\n' );
+		res.end();
+		return;
+	}
+	
+	/* Check if it's a file or directory */
+	if ( stats.isFile() )
+	{
+		/* Get mimeType */
+		var mimeType = mimeTypes[path.extname( fileName ).split( "." ).reverse()[0]];
+		res.writeHead( 200, {'Content-type': mimeType} );
+		
+		var fileStream = fs.createReadStream( fileName );
+		fileStream.pipe( res );
+	}
+	else if( stats.isDirectory() )
+	{
+		/* Redirect to home directory page */
+		res.writeHead( 302, {'Location': 'index.html'} );
+		res.end();
+	}
+	else
+	{
+		/* Internal Error */
+		res.writeHead( 500, {'Content-type' : 'text/plain'} );
+		res.write( '500 Internal Error\n' );
+		res.end();
+	}
+	
+}).listen( 3000 );
